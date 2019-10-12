c263470bdb535e71737e50364411979224c7d9b1
aey76 <w8.pc@hotmail.com>
2019-10-12 23:42:52 +0300

    Update testing_static_ini_reader.h

--- test/testing_static_ini_reader.h
+++ test/testing_static_ini_reader.h
@@ -2,4 +2,15 @@
 // File Name: testing_static_ini_reader.h
 // #############################################################################
 
+
+class testing_static_ini_reader
+{
+public:
+  bool isReady() const;
+  bool loadFile(const char* iniFileToLoad);
+  
+  IniVar<int> var_1;
+};
+
+
 // --- END OF FILE ---
